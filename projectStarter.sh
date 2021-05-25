fce35fc10d31833b13c2fe8e1c42270bf01cf3c0
Aldwin <aldwin828@gmail.com>
2021-05-25 16:01:11 -0400

    Started skeleton, added images and video to homepage

---
+++ projectStarter.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+mkdir 'basicbash';
+touch ./basicbash/index.html;
+echo 'Creating a new project';
+echo 'Hello world' > ./basicbash/index.html;
+mkdir css;
+touch ./css/style.css;
+mkdir ./basicbash/js;
+touch ./basicbash/app.js;
+echo 'All done!';
+mv ./basicbash/ ./OkotoksPizza;
